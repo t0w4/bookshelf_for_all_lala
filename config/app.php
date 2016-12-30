3e07c8f113e141d36959a148fb7f339362d61131
tokunagawataru <tokunaga.wataru31@gmail.com>
2016-12-30 17:53:12 +0900

    IDE-helper_generate

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         // ...
         Collective\Html\HtmlServiceProvider::class,
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider', // Laravel IDE helper
         // ...
 
     ],
