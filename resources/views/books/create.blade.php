1b028a62d940d6d22f4c27703a99c702e201b9f4
tokunagawataru <tokunaga.wataru31@gmail.com>
2017-01-05 16:50:29 +0900

    books_create_func_fix
    登録エラーの時、直前に入力した内容を表示する

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -18,19 +18,19 @@
     {{ Form::open(['url' => 'books', 'method' => 'POST']) }}
       <div class="form-group">
         <label class="control-label" for="focusedInput_title">作品名</label>
-        <input class="form-control" id="focusedInput_title" type="text" name="book[title]">
+        <input class="form-control" id="focusedInput_title" type="text" value="{{ old('book.title') }}" name="book[title]">
       </div>
       <div class="form-group">
         <label class="control-label" for="focusedInput_author">著者</label>
-        <input class="form-control" id="focusedInput_author" type="text" name="book[author]">
+        <input class="form-control" id="focusedInput_author" type="text" value="{{ old('book.author') }}" name="book[author]">
       </div>
       <div class="form-group">
         <label class="control-label" for="focusedInput_publisher">出版社</label>
-        <input class="form-control" id="focusedInput_publisher" type="text" name="book[publisher]">
+        <input class="form-control" id="focusedInput_publisher" type="text" value="{{ old('book.publisher') }}" name="book[publisher]">
       </div>
       <div class="form-group">
         <label class="control-label" for="focusedInput_publicationDate">出版日(年/月/日)</label>
-        <input class="form-control" id="focusedInput_publicationDate" type="text" name="book[publicationDate]">
+        <input class="form-control" id="focusedInput_publicationDate" type="text" value="{{ old('book.publicationDate') }}" name="book[publicationDate]">
       </div>
       <div class="form-group">
         <label class="control-label" for="focusedInput_image">画像URL</label>
@@ -39,12 +39,12 @@
 
       <div class="form-group">
         <label class="control-label" for="focusedInput_tags">タグ(入力後Enter)</label><br />
-        <input type="text" name="book[tag_list]" id="focusedInput_tags" value="" class="form-control" data-role="tagsinput" style="display: none;">
+        <input type="text" name="book[tag_list]" id="focusedInput_tags" value="{{ old('book.tag_list') }}" class="form-control" data-role="tagsinput" style="display: none;">
       </div>
 
       <div class="form-group">
         <label class="control-label" for="focusedInput_description">本の概要</label>
-        <textarea class="form-control" id="focusedInput_description" name="book[description]"></textarea>
+        <textarea class="form-control" id="focusedInput_description" name="book[description]">{{ old('book.description') }}</textarea>
       </div>
 
       <p class=show-btn>
